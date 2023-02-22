f56fbfefe6659065ea7c3008f4bbd2e0acd197af
Christian Felix <1christianfelix@gmail.com>
2023-02-22 12:54:40 -0800

    Added photo_url in the ListLocationEncoder to get picture_url data for updating card

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     console.log(response);
     if (!response.ok) {
       // Throwing an error to catch
-      throw new Error("Response not ok");
+      throw new Error(response.status);
     } else {
       const data = await response.json();
       // console.log(data);
@@ -15,18 +15,39 @@ window.addEventListener("DOMContentLoaded", async () => {
       const nameTag = document.querySelector(".card-title");
       nameTag.innerHTML = conference.name;
 
+      // Getting conference details
       const detailUrl = `http://localhost:8000${conference.href}`;
       const detailResponse = await fetch(detailUrl);
       if (detailResponse.ok) {
         const details = await detailResponse.json();
-        // console.log(details);
+        console.log(details);
 
         const conferenceDescription = details.conference.description;
         const descriptionTag = document.querySelector(".card-text");
         descriptionTag.innerHTML = conferenceDescription;
+
+        const conferencePicture_Url = details.conference.location.picture_url;
+        const pictureTag = document.querySelector(".card-img-top");
+        pictureTag.src = conferencePicture_Url;
       }
     }
   } catch (e) {
     console.error("error", e);
   }
 });
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   const url = "http://localhost:8000/api/conferences/";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       // Figure out what to do when the response is bad
+//       throw new Error(response.status);
+//     } else {
+//       const data = await response.json();
+//     }
+//   } catch (e) {
+//     // Figure out what to do if an error is raised
+//     console.error("error", e);
+//   }
+// });
