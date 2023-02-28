cc87a9e3102a9d269bce54dd9d36e78684873906
Christian Felix <1christianfelix@gmail.com>
2023-02-28 14:49:14 -0800

    Added a Location Form and AttendeeList Table in React App

---
+++ ghi/app/src/LocationForm.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+
+function LocationForm() {
+  const [states, setStates] = useState([]);
+  const [name, setName] = useState("");
+  const [roomCount, setRoomCount] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+  const handleRoomCountChange = (event) => {
+    const value = event.target.value;
+    setRoomCount(value);
+  };
+  const handleCityChange = (event) => {
+    const value = event.target.value;
+    setCity(value);
+  };
+  const handleStateChange = (event) => {
+    const value = event.target.value;
+    setState(value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      room_count: roomCount,
+      name: name,
+      city: city,
+      state: state,
+    };
+    console.log("test");
+    console.log(data);
+
+    const locationUrl = "http://localhost:8000/api/locations/";
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      const newLocation = await response.json();
+      console.log(newLocation);
+      setName("");
+      setRoomCount("");
+      setCity("");
+      setState("");
+    }
+  };
+
+  const fetchData = async () => {
+    const url = "http://localhost:8000/api/states/";
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      setStates(data.states);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Create a new location</h1>
+            <form onSubmit={handleSubmit} id="create-location-form">
+              <div className="form-floating mb-3">
+                <input
+                  name="name"
+                  placeholder="Name"
+                  required
+                  type="text"
+                  id="name"
+                  className="form-control"
+                  onChange={handleNameChange}
+                  value={name}
+                />
+                <label htmlFor="name">Test</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  name="room_count"
+                  placeholder="Room count"
+                  required
+                  type="number"
+                  id="room_count"
+                  className="form-control"
+                  onChange={handleRoomCountChange}
+                  value={roomCount}
+                />
+                <label htmlFor="room_count">Room count</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  name="city"
+                  placeholder="City"
+                  required
+                  type="text"
+                  id="city"
+                  className="form-control"
+                  onChange={handleCityChange}
+                  value={city}
+                />
+                <label htmlFor="city">City</label>
+              </div>
+              <div className="mb-3">
+                <select
+                  required
+                  name="state"
+                  id="state"
+                  className="form-select"
+                  onChange={handleStateChange}
+                  value={state}
+                >
+                  <option value="">Choose a state</option>
+                  {states.map((state) => {
+                    return (
+                      <option
+                        key={state.abbreviation}
+                        value={state.abbreviation}
+                      >
+                        {state.name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <button className="btn btn-primary">Create</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LocationForm;
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -3,14 +3,14 @@ import React from "react";
 const Nav = () => {
   return (
     <header>
-      <div class="fixed-top" id="liveAlertPlaceholder"></div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <div className="fixed-top" id="liveAlertPlaceholder"></div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Conference GO!
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -18,36 +18,36 @@ const Nav = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <a
-                  class="nav-link  "
+                  className="nav-link  "
                   aria-current="page"
                   href="new-location.html"
                 >
                   New location
                 </a>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <a
-                  class="nav-link d-none"
+                  className="nav-link d-none"
                   aria-current="page"
                   href="new-conference.html"
                 >
                   New conference
                 </a>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <a
-                  class="nav-link"
+                  className="nav-link"
                   aria-current="page"
                   href="attend-conference.html"
                 >
